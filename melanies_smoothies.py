a292735e5236577bdd6e787257c0be6c64adb951
Yves St-Hilaire <yves.sthilaire@gmail.com>
2024-07-01 16:18:36 -0400

    changement connexion

--- melanies_smoothies.py
+++ melanies_smoothies.py
@@ -1,46 +1,46 @@
-    # Import python packages
-    import streamlit as st
-    from snowflake.snowpark.context import get_active_session
-    from snowflake.snowpark.functions import col;
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col;
 
-    # Write directly to the app
-    st.title(":cup_with_straw: Super smoothies order form")
-    st.write(
-        """
-        Choose your fruits and stuff.
-        """
-    )
+# Write directly to the app
+st.title(":cup_with_straw: Super smoothies order form")
+st.write(
+    """
+    Choose your fruits and stuff.
+    """
+)
 
 
-    #option = st.selectbox(
-    #    "What's your favorite fruit?",
-    #    ("Banana", "Strawberries", "Peaches"))
-    #
-    #st.write("Your favorite fruit is :", option);
+#option = st.selectbox(
+#    "What's your favorite fruit?",
+#    ("Banana", "Strawberries", "Peaches"))
+#
+#st.write("Your favorite fruit is :", option);
 
-    name_on_order = st.text_input("Name on smoothie:")
+name_on_order = st.text_input("Name on smoothie:")
 
-    session = get_active_session()
-    my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'));
-    #st.dataframe(data=my_dataframe, use_container_width=True);
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'));
+#st.dataframe(data=my_dataframe, use_container_width=True);
 
-    ingredients_list = st.multiselect("Choose up to 5 fruits", my_dataframe, max_selections=5);
+ingredients_list = st.multiselect("Choose up to 5 fruits", my_dataframe, max_selections=5);
 
-    ingredients_string = ''
-    my_insert_stmt = ''
-    if ingredients_list:
-        st.write(ingredients_list);
-        st.text(ingredients_list);
+ingredients_string = ''
+my_insert_stmt = ''
+if ingredients_list:
+    st.write(ingredients_list);
+    st.text(ingredients_list);
 
-        ingredients_string = ' '.join(ingredients_list);
-        my_insert_stmt = f"insert into smoothies.public.orders (ingredients, name_on_order) values ('{ingredients_string}', '{name_on_order}')";
+    ingredients_string = ' '.join(ingredients_list);
+    my_insert_stmt = f"insert into smoothies.public.orders (ingredients, name_on_order) values ('{ingredients_string}', '{name_on_order}')";
 
-    st.write(ingredients_string);
-    st.write(my_insert_stmt);
+st.write(ingredients_string);
+st.write(my_insert_stmt);
 
-    time_to_insert = st.button("Order");
-    if ingredients_string:
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect();
-            st.success('Your Smoothie is ordered!', icon="✅");
-        
+time_to_insert = st.button("Order");
+if ingredients_string:
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect();
+        st.success('Your Smoothie is ordered!', icon="✅");
+    
